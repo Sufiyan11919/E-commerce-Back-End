d0333b0f6800e9dd93adee398ddb05278b67390d
sufi <sufiyanshaikh11919@gmail.com>
2022-10-22 17:06:15 -0700

    done with models

--- models/index.js
+++ models/index.js
@@ -4,11 +4,24 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+
+// adding relations
 // Products belongsTo Category
+Product.belongsTo(Category,{
+  foreignKey:"category_id"
+})
 
 // Categories have many Products
 
+Category.hasMany(Product,{
+  foreignKey:"category_id"
+})
+
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag,{
+  through: ProductTag,
+  foreignKey: 'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
 
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,13 +6,21 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+ Product.findAll({
+  include :[{
+    model : Category
+  }]
+
+})
+.then(data=>{
+  res.json(data)
+})
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+ 
 });
 
 // create new product
